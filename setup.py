b2cdae7363dc9d0cac09d98f3d79102477d6c331
Zachary Spector <zacharyspector@gmail.com>
2014-08-07 19:40:34 -0400

    change license from GPL3 to BSD

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 setup(
     name = "gorm",
-    version = "0.2.4",
+    version = "0.2.5",
     packages = ["gorm"],
     install_requires = ['networkx>=1.9'],
     author = "Zachary Spector",
     author_email = "zacharyspector@gmail.com",
     description = "An object-relational mapper serving database-backed versions of the standard networkx graph classes.",
-    license = "GPL3",
+    license = "BSD",
     keywords = "orm graph networkx sql database",
     url = "https://github.com/LogicalDash/gorm"
 )
