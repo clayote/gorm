efae3799603833e80baea8048f83f8a9f462dace
Zachary Spector <zacharyspector@gmail.com>
2014-09-02 18:02:30 -0400

    typo

--- gorm/graph.py
+++ gorm/graph.py
@@ -37,7 +37,7 @@ def dec_tuple(o):
         if o[0] == 'list':
             return list(dec_tuple(p) for p in o[1:])
         else:
-            assert(o[0]) == 'tuple'
+            assert(o[0] == 'tuple')
             return tuple(dec_tuple(p) for p in o[1:])
     else:
         return o
@@ -2138,3 +2138,35 @@ class MultiDiGraph(networkx.MultiDiGraph, GormGraph):
             (u, v) = e[:2]
             if u in self.succ and v in self.succ[u]:
                 del self.succ[u][v]
+
+    def add_edge(self, u, v, key=None, attr_dict=None, **attr):
+        """Version of add_edge that only writes to the database once."""
+        if attr_dict is None:
+            attr_dict = attr
+        else:
+            try:
+                attr_dict.update(attr)
+            except AttributeError:
+                raise NetworkXError(
+                    "The attr_dict argument must be a dictionary."
+                )
+        if u not in self.node:
+            self.node[u] = {}
+        if v not in self.node:
+            self.node[v] = {}
+        if v in self.succ[u]:
+            keydict = self.adj[u][v]
+            if key is None:
+                key = len(keydict)
+                while key in keydict:
+                    key += 1
+            datadict = keydict.get(key, {})
+            datadict.update(attr_dict)
+            keydict[key] = datadict
+        else:
+            if key is None:
+                key = 0
+            datadict = {}
+            datadict.update(attr_dict)
+            keydict = {key: datadict}
+            self.succ[u][v] = keydict
