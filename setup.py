10bb138289d8aeec7f06360f1e18f8e2579a40cc
Zachary Spector <zacharyspector@gmail.com>
2015-01-05 20:43:17 -0500

    I guess I have to update this again

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "gorm",
-    version = "0.5.7",
+    version = "0.5.8",
     packages = ["gorm"],
     install_requires = ['networkx>=1.9'],
     author = "Zachary Spector",
