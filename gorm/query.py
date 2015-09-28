b6bb4f41f68d24bb45b1c4a7dadf0070ce995a70
Zachary Spector <zacharyspector@gmail.com>
2015-09-27 20:36:04 -0400

    allow custom json dumper and loader

--- gorm/query.py
+++ gorm/query.py
@@ -14,11 +14,8 @@ try:
 except ImportError:
     # python 3
     from gorm import xjson
-json_dump = xjson.json_dump
-json_load = xjson.json_load
 import os
 xjpath = os.path.dirname(xjson.__file__)
-
 alchemyIntegError = None
 try:
     from sqlalchemy.exc import IntegrityError as alchemyIntegError
@@ -70,7 +67,7 @@ class QueryEngine(object):
     """
     json_path = xjpath
 
-    def __init__(self, dbstring, connect_args, alchemy):
+    def __init__(self, dbstring, connect_args, alchemy, json_dump=None, json_load=None):
         """If ``alchemy`` is True and ``dbstring`` is a legit database URI,
         instantiate an Alchemist and start a transaction with
         it. Otherwise use sqlite3.
@@ -117,6 +114,8 @@ class QueryEngine(object):
             lite_init(dbstring, connect_args)
 
         self._branches = {}
+        self.json_dump = json_dump if json_dump else xjson.json_dump
+        self.json_load = json_load if json_load else xjson.json_load
 
     @reify
     def globl(self):
@@ -155,23 +154,23 @@ class QueryEngine(object):
         while branch != 'master':
             if branch not in self._branches:
                 (b, r) = self.parparrev(branch)
-                self._branches[branch] = (b, json_load(r))
+                self._branches[branch] = (b, self.json_load(r))
             (branch, rev) = self._branches[branch]
             yield (branch, rev)
 
     def have_graph(self, graph):
         """Return whether I have a graph by this name."""
-        graph = json_dump(graph)
+        graph = self.json_dump(graph)
         return bool(self.sql('ctgraph', graph).fetchone()[0])
 
     def new_graph(self, graph, typ):
         """Declare a new graph by this name of this type."""
-        graph = json_dump(graph)
+        graph = self.json_dump(graph)
         return self.sql('new_graph', graph, typ)
 
     def del_graph(self, graph):
         """Delete all records to do with the graph"""
-        g = json_dump(graph)
+        g = self.json_dump(graph)
         self.sql('del_edge_val_graph', g)
         self.sql('del_edge_graph', g)
         self.sql('del_node_val_graph', g)
@@ -180,7 +179,7 @@ class QueryEngine(object):
 
     def graph_type(self, graph):
         """What type of graph is this?"""
-        graph = json_dump(graph)
+        graph = self.json_dump(graph)
         return self.sql('graph_type', graph).fetchone()[0]
 
     def have_branch(self, branch):
@@ -196,23 +195,23 @@ class QueryEngine(object):
 
     def global_get(self, key):
         """Return the value for the given key in the ``globals`` table."""
-        key = json_dump(key)
+        key = self.json_dump(key)
         r = self.sql('global_get', key).fetchone()
         if r is None:
             raise KeyError("Not set")
-        return json_load(r[0])
+        return self.json_load(r[0])
 
     def global_items(self):
         """Iterate over (key, value) pairs in the ``globals`` table."""
         for (k, v) in self.sql('global_items'):
-            yield (json_load(k), json_load(v))
+            yield (self.json_load(k), self.json_load(v))
 
     def global_set(self, key, value):
         """Set ``key`` to ``value`` globally (not at any particular branch or
         revision)
 
         """
-        (key, value) = map(json_dump, (key, value))
+        (key, value) = map(self.json_dump, (key, value))
         try:
             return self.sql('global_ins', key, value)
         except IntegrityError:
@@ -220,7 +219,7 @@ class QueryEngine(object):
 
     def global_del(self, key):
         """Delete the global record for the key."""
-        key = json_dump(key)
+        key = self.json_dump(key)
         return self.sql('global_del', key)
 
     def parrev(self, branch):
@@ -243,7 +242,7 @@ class QueryEngine(object):
         revision.
 
         """
-        graph = json_dump(graph)
+        graph = self.json_dump(graph)
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             data = self.sql(
@@ -251,7 +250,7 @@ class QueryEngine(object):
             )
             for (k, v) in data:
                 if k not in seen:
-                    yield json_load(k)
+                    yield self.json_load(k)
                 seen.add(k)
 
     def graph_val_get(self, graph, key, branch, rev):
@@ -259,7 +258,7 @@ class QueryEngine(object):
         revision.
 
         """
-        (graph, key) = map(json_dump, (graph, key))
+        (graph, key) = map(self.json_dump, (graph, key))
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
                 'graph_val_get',
@@ -270,12 +269,12 @@ class QueryEngine(object):
             ):
                 if row is None:
                     raise KeyError("Key not set")
-                return json_load(row[0])
+                return self.json_load(row[0])
         raise KeyError("Key never set")
 
     def graph_val_set(self, graph, key, branch, rev, value):
         """Set a key to a value on a graph at a particular revision."""
-        (graph, key, value) = map(json_dump, (graph, key, value))
+        (graph, key, value) = map(self.json_dump, (graph, key, value))
         try:
             self.sql('graph_val_ins', graph, key, branch, rev, value)
         except IntegrityError:
@@ -283,7 +282,7 @@ class QueryEngine(object):
 
     def graph_val_del(self, graph, key, branch, rev):
         """Indicate that the key is unset."""
-        (graph, key) = map(json_dump, (graph, key))
+        (graph, key) = map(self.json_dump, (graph, key))
         try:
             self.sql('graph_val_ins', graph, key, branch, rev, None)
         except IntegrityError:
@@ -294,7 +293,7 @@ class QueryEngine(object):
         revision.
 
         """
-        graph = json_dump(graph)
+        graph = self.json_dump(graph)
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             data = self.sql(
@@ -305,7 +304,7 @@ class QueryEngine(object):
             )
             for (n,) in data:
                 if n is not None and n not in seen:
-                    yield json_load(n)
+                    yield self.json_load(n)
                 seen.add(n)
 
     def node_exists(self, graph, node, branch, rev):
@@ -313,7 +312,7 @@ class QueryEngine(object):
         revision.
 
         """
-        (graph, node) = map(json_dump, (graph, node))
+        (graph, node) = map(self.json_dump, (graph, node))
         for (b, r) in self.active_branches(branch, rev):
             for x in self.sql(
                 'node_exists', graph, node, b, r
@@ -327,7 +326,7 @@ class QueryEngine(object):
         Inserts a new record or updates an old one, as needed.
 
         """
-        (graph, node) = map(json_dump, (graph, node))
+        (graph, node) = map(self.json_dump, (graph, node))
         try:
             self.sql('exist_node_ins', graph, node, branch, rev, extant)
         except IntegrityError:
@@ -338,7 +337,7 @@ class QueryEngine(object):
         revision.
 
         """
-        (graph, node) = map(json_dump, (graph, node))
+        (graph, node) = map(self.json_dump, (graph, node))
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             for (k, v) in self.sql(
@@ -349,20 +348,20 @@ class QueryEngine(object):
                     rev
             ):
                 if k not in seen and v is not None:
-                    yield json_load(k)
+                    yield self.json_load(k)
                 seen.add(k)
 
     def node_vals_ever(self, graph, node):
         """Iterate over all values set on a node through time."""
-        (graph, node) = map(json_dump, (graph, node))
+        (graph, node) = map(self.json_dump, (graph, node))
         for (key, branch, tick, value) in self.sql(
                 'node_vals_ever', graph, node
         ):
-            yield (json_load(key), branch, tick, json_load(value))
+            yield (self.json_load(key), branch, tick, self.json_load(value))
 
     def node_val_get(self, graph, node, key, branch, rev):
         """Get the value of the node's key as it was at the given revision."""
-        (graph, node, key) = map(json_dump, (graph, node, key))
+        (graph, node, key) = map(self.json_dump, (graph, node, key))
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
                 'node_val_get',
@@ -374,13 +373,13 @@ class QueryEngine(object):
             ):
                 if row[0] is None:
                     raise KeyError("Key not set")
-                return json_load(row[0])
+                return self.json_load(row[0])
         raise KeyError("Key {} never set".format(key))
 
     def node_val_set(self, graph, node, key, branch, rev, value):
         """Set the value of a key on a node at a particular revision."""
         (graph, node, key, value) = map(
-            json_dump,
+            self.json_dump,
             (graph, node, key, value)
         )
         try:
@@ -394,7 +393,7 @@ class QueryEngine(object):
 
     def node_val_del(self, graph, node, key, branch, rev):
         """Indicate that the key has no value for the node at the revision."""
-        (graph, node, key) = map(json_dump, (graph, node, key))
+        (graph, node, key) = map(self.json_dump, (graph, node, key))
         try:
             return self.sql(
                 'node_val_ins', graph, node, key, branch, rev, None
@@ -409,14 +408,14 @@ class QueryEngine(object):
         graph, at this revision.
 
         """
-        graph = json_dump(graph)
+        graph = self.json_dump(graph)
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
                 'edges_extant', graph, branch, rev
             ):
                 if row[0] not in seen and row[1]:
-                    yield json_load(row[0])
+                    yield self.json_load(row[0])
                 seen.add(row[0])
 
     def edge_exists(self, graph, nodeA, nodeB, idx, branch, rev):
@@ -424,7 +423,7 @@ class QueryEngine(object):
         about it in this branch.
 
         """
-        (graph, nodeA, nodeB) = map(json_dump, (graph, nodeA, nodeB))
+        (graph, nodeA, nodeB) = map(self.json_dump, (graph, nodeA, nodeB))
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
                 'edge_exists',
@@ -443,7 +442,7 @@ class QueryEngine(object):
         node.
 
         """
-        (graph, nodeB) = map(json_dump, (graph, nodeB))
+        (graph, nodeB) = map(self.json_dump, (graph, nodeB))
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
@@ -454,19 +453,19 @@ class QueryEngine(object):
                 r
             ):
                 if row[0] not in seen and row[1]:
-                    yield json_load(row[0])
+                    yield self.json_load(row[0])
                 seen.add(row[0])
 
     def nodeBs(self, graph, nodeA, branch, rev):
         """Return an iterable of nodes you can get to from the given one."""
-        (graph, nodeA) = map(json_dump, (graph, nodeA))
+        (graph, nodeA) = map(self.json_dump, (graph, nodeA))
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
                 'nodeBs', graph, nodeA, b, r
             ):
                 if row[0] not in seen and row[1]:
-                    yield json_load(row[0])
+                    yield self.json_load(row[0])
                 seen.add(row[0])
 
     def multi_edges(self, graph, nodeA, nodeB, branch, rev):
@@ -474,7 +473,7 @@ class QueryEngine(object):
         nodes.
 
         """
-        (graph, nodeA, nodeB) = map(json_dump, (graph, nodeA, nodeB))
+        (graph, nodeA, nodeB) = map(self.json_dump, (graph, nodeA, nodeB))
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
@@ -486,7 +485,7 @@ class QueryEngine(object):
 
     def exist_edge(self, graph, nodeA, nodeB, idx, branch, rev, extant):
         """Declare whether or not this edge exists."""
-        (graph, nodeA, nodeB) = map(json_dump, (graph, nodeA, nodeB))
+        (graph, nodeA, nodeB) = map(self.json_dump, (graph, nodeA, nodeB))
         try:
             self.sql(
                 'edge_exist_ins', graph, nodeA, nodeB, idx, branch, rev, extant
@@ -498,32 +497,32 @@ class QueryEngine(object):
 
     def edge_val_keys(self, graph, nodeA, nodeB, idx, branch, rev):
         """Return an iterable of keys this edge has."""
-        (graph, nodeA, nodeB) = map(json_dump, (graph, nodeA, nodeB))
+        (graph, nodeA, nodeB) = map(self.json_dump, (graph, nodeA, nodeB))
         seen = set()
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
                 'edge_val_items', graph, nodeA, nodeB, idx, b, r
             ):
                 if row[0] not in seen:
-                    yield json_load(row[0])
+                    yield self.json_load(row[0])
                 seen.add(row[0])
 
     def edge_val_get(self, graph, nodeA, nodeB, idx, key, branch, rev):
         """Return the value of this key of this edge."""
-        (graph, nodeA, nodeB, key) = map(json_dump, (graph, nodeA, nodeB, key))
+        (graph, nodeA, nodeB, key) = map(self.json_dump, (graph, nodeA, nodeB, key))
         for (b, r) in self.active_branches(branch, rev):
             for row in self.sql(
                 'edge_val_get', graph, nodeA, nodeB, idx, key, b, r
             ):
                 if row[0] is None:
                     raise KeyError("Key not set")
-                return json_load(row[0])
+                return self.json_load(row[0])
         raise KeyError("Key never set")
 
     def edge_val_set(self, graph, nodeA, nodeB, idx, key, branch, rev, value):
         """Set this key of this edge to this value."""
         (graph, nodeA, nodeB, key, value) = map(
-            json_dump,
+            self.json_dump,
             (graph, nodeA, nodeB, key, value)
         )
         try:
@@ -557,7 +556,7 @@ class QueryEngine(object):
 
         """
         (graph, nodeA, nodeB, key) = map(
-            json_dump,
+            self.json_dump,
             (graph, nodeA, nodeB, key)
         )
         try:
