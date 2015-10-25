1226ff507a050749d2aafb56e338f8a5ed2352a2
Zachary Spector <zacharyspector@gmail.com>
2015-10-25 11:51:16 -0400

    Blank __init__ because my fancy one was ruining imports

--- gorm/__init__.py
+++ gorm/__init__.py
@@ -1,6 +1,1 @@
-# This file is part of gorm, an object relational mapper for versioned graphs.
-# Copyright (C) 2014 Zachary Spector.
-from .gorm import ORM
-from . import graph
 
-__all__ = [ORM, graph]
