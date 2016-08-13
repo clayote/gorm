f854719c566252b4e26de9f6ef37fef224852934
Zachary Spector <zacharyspector@gmail.com>
2016-08-13 18:37:28 -0400

    Redo WindowDict with a better algorithm and no numpy dependency
    Algorithm modeled on the one presented by Jeremiah Reid at the
    International Roguelike Developer's Conference 2016 in
    his talk "Roguelike Time Traveling"
    https://www.twitch.tv/roguelike_con/v/82270325

--- gorm/window.py
+++ gorm/window.py
@@ -1,82 +1,121 @@
-from collections import defaultdict, MutableMapping
-try:
-    from numpy import array, less_equal, greater
-    have_numpy = True
-except ImportError:
-    have_numpy = False
-
-
-def window_left(revs, rev):
-    k = frozenset(revs)
-    if k not in window_left.memo or rev not in window_left.memo[k]:
-        if have_numpy:
-            revs = array(tuple(k))
-            smalls = revs[less_equal(revs, rev)]
-            if len(smalls):
-                window_left.memo[k][rev] = smalls.max()
-            else:
-                window_left.memo[k][rev] = None
-        else:
-            smalls = [rv for rv in revs if rv < rev]
-            if smalls:
-                window_left.memo[k][rev] = max(smalls)
-            else:
-                window_left.memo[k][rev] = None
-    return window_left.memo[k][rev]
-window_left.memo = defaultdict(dict)
-
-
-def window_right(revs, rev):
-    k = frozenset(revs)
-    if k not in window_right.memo or rev not in window_right.memo[k]:
-        if have_numpy:
-            revs = array(tuple(k))
-            bigs = revs[greater(revs, rev)]
-            if len(bigs):
-                window_right.memo[k][rev] = bigs.min()
-            else:
-                window_right.memo[k][rev] = None
-        else:
-            bigs = [rv for rv in revs if rv > rev]
-            if bigs:
-                window_right.memo[k][rev] = min(bigs)
-            else:
-                window_right.memo[k][rev] = None
-    return window_right.memo[k][rev]
-window_right.memo = defaultdict(dict)
-
-
-def window(revs, rev):
-    return (
-        window_left(revs, rev),
-        window_right(revs, rev)
-    )
+from collections import deque, MutableMapping
 
 
 class WindowDict(MutableMapping):
-    def __init__(self):
-        self._real = {}
+    """A dict that keeps every value that a variable has had over time.
+    
+    Look up a revision number in this dict and it will give you the effective value as
+    of that revision. Keys should always be revision numbers.
+    
+    Optimized for the cases where you look up the same revision repeatedly, or its neighbors.
+    
+    """
+    def seek(self, rev):
+        """Arrange the caches in the optimal way for looking up the given revision."""
+        while self._future and self._future[0][0] < rev:
+            self._past.append(self._future.popleft())
+        while self._past and self._past[-1][0] > rev:
+            self._future.appendleft(self._past.pop())
+
+    def rev_before(self, rev):
+        """Return the last rev prior to the given one on which the value changed."""
+        self.seek(rev)
+        return self._past[-1][0]
+
+    def rev_after(self, rev):
+        """Return the next rev after the given one on which the value will change."""
+        self.seek(rev)
+        return self._future[0][0]
+
+    def __init__(self, data={}):
+        self._past = deque()
+        self._future = deque()
+        for (rev, v) in sorted(data.items()):
+            if rev <= self._rev:
+                self._past.append((rev, v))
+            else:
+                self._future.append((rev, v))
 
     def __iter__(self):
-        return iter(self._real)
+        for (rev, v) in self._past:
+            yield rev
+        for (rev, v) in self._future:
+            yield rev
 
     def __len__(self):
-        return len(self._real)
+        return len(self._past) + len(self._future)
 
-    def __setitem__(self, k, v):
-        self._real[k] = v
+    def __getitem__(self, rev):
+        latest = self._past.pop()
+        if rev == latest[0]:
+            self._past.append(latest)
+            return latest[1]
+        elif rev > latest[0]:
+            next = self._future.popleft()
+            if rev < next[0]:
+                self._future.appendleft(next)
+                self._past.append(latest)
+                return latest
+            future = self._future
+            self._future = deque([next])
+            while rev < next[0]:
+                next = future.popleft()
+                self._future.append(next)
+            self._future.extend(future)
+            return next[1]
+        else:  # rev < latest[0]
+            past = self._past
+            less_past = deque([latest])
+            while rev < latest[0]:
+                try:
+                    latest = past.pop()
+                except IndexError:
+                    self._past = less_past
+                    raise KeyError("Revision {} is before the start of history".format(rev))
+                less_past.appendleft(latest)
+            self._past = past + less_past
+            return latest[1]
 
-    def __delitem__(self, k):
-        del self._real[k]
-
-    def __getitem__(self, k):
-        if k in self._real:
-            return self._real[k]
-        try:
-            return self._real[
-                window_left(self._real.keys(), k)
-            ]
-        except ValueError:
-            raise KeyError(
-                "Key {} not set, nor any before it.".format(k)
-            )
+    def __setitem__(self, rev, v):
+        if rev < self._past[0][0]:
+            self._past.appendleft((rev, v))
+        elif rev == self._past[0][0]:
+            self._past[0] = (rev, v)
+        elif rev == self._past[-1][0]:
+            self._past[-1] = (rev, v)
+        elif rev > self._past[-1][0]:
+            if rev < self._future[0][0]:
+                self._past.append((rev, v))
+            elif rev == self._future[0][0]:
+                self._future[0][0] = (rev, v)
+            elif rev == self._future[-1][0]:
+                self._future[-1][0] = (rev, v)
+            elif rev > self._future[-1][0]:
+                self._future.append((rev, v))
+            else:
+                self._future.append((rev, v))
+                inserted = sorted(self._future)
+                self._future = deque(inserted)
+        else:
+            # I was going to implement my own insertion sort here, but I gather Python already
+            # does that, via Timsort. I wonder if there's a way I can give it a hint, so it doesn't
+            # have to check for partial ordering? And maybe avoid reconstructing the deque?
+            self._past.append((rev, v))
+            inserted = sorted(self._past)
+            self._past = deque(inserted)
+    
+    def __delitem__(self, rev):
+        name = '_past' if rev <= self._rev else '_future'
+        stack = getattr(self, name)
+        waste = deque()
+        setattr(self, name, waste)
+        deleted = False
+        while stack:
+            (r, v) = stack.popleft()
+            if r != rev:
+                waste.append((r, v))
+            else:
+                assert not deleted
+                deleted = True
+        if not deleted:
+            raise KeyError("Rev not present: {}".format(rev))
