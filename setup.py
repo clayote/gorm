eddaf0cc6c885b9995dd772b394606e77700dbf4
Zachary Spector <zacharyspector@gmail.com>
2016-08-19 16:41:31 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "gorm",
-    version = "0.8.1",
+    version = "0.8.2",
     packages = ["gorm"],
     install_requires = ['networkx>=1.9'],
     author = "Zachary Spector",
