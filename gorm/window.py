5db382333884b00b090c392b23bd3c0a7313c50f
Zachary Spector <zacharyspector@gmail.com>
2016-08-21 11:35:25 -0400

    Make ``WindowDict.seek`` go past the revision you're seeking
    This makes it possible to get data from that very revision.

--- gorm/window.py
+++ gorm/window.py
@@ -14,7 +14,7 @@ class WindowDict(MutableMapping):
     """
     def seek(self, rev):
         """Arrange the caches in the optimal way for looking up the given revision."""
-        while self._future and self._future[0][0] < rev:
+        while self._future and self._future[0][0] <= rev:
             self._past.append(self._future.popleft())
         while self._past and self._past[-1][0] > rev:
             self._future.appendleft(self._past.pop())
