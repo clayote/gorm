715e7fc46d592b0c09745c7523a72176948add9a
Zachary Spector <zacharyspector@gmail.com>
2016-08-25 22:33:37 -0400

    Remove test_graph_atlas
    It was slow and wasn't really telling me anything I couldn't
    get from test_branch_lineage.

--- test.py
+++ test.py
@@ -144,24 +144,6 @@ class GormTest(unittest.TestCase):
                 )
             )
 
-    def test_graph_atlas(self):
-        """Test saving and loading all the graphs in the networkx graph
-        atlas.
-
-        """
-        for g in graph_atlas_g():
-            if int(g.name[1:]) % 100 == 0:
-                print(g.name)
-            gormg = self.engine.new_graph(g.name, g)
-            for n in g.node:
-                self.assertIn(n, gormg.node)
-                self.assertEqual(g.node[n], gormg.node[n])
-            for u in g.edge:
-                for v in g.edge[u]:
-                    self.assertIn(u, gormg.edge)
-                    self.assertIn(v, gormg.edge[u])
-                    self.assertEqual(g.edge[u][v], gormg.edge[u][v])
-
 
 if __name__ == '__main__':
     unittest.main()
