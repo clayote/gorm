15a4b62c1d22c01d6beddddbdbfdee7b4a3fa12a
Zachary Spector <zacharyspector@gmail.com>
2016-10-07 14:54:54 -0400

    Redux WindowDict to use Trique instead of deque

--- gorm/window.py
+++ gorm/window.py
@@ -1,44 +1,27 @@
-from collections import deque, MutableMapping, ItemsView, ValuesView
+from collections import ItemsView, ValuesView, MutableMapping
+from .trique import Trique
 
 
 class WindowDictItemsView(ItemsView):
     def __contains__(self, item):
-        (rev, v) = item
-        if self._mapping._past:
-            if rev < self._mapping._past[0][0]:
-                return False
-        elif self._mapping._future:
-            if rev < self._mapping._future[0][0]:
-                return False
-        else:
-            return False
-        for mrev, mv in self._mapping._past:
-            if mrev == rev:
-                return mv == v
-        for mrev, mv in self._mapping._future:
-            if mrev == rev:
-                return mv == v
+        for (rev, v) in self._mapping.history:
+            if (rev, v) == item:
+                return True
         return False
 
     def __iter__(self):
-        yield from self._mapping._past
-        yield from self._mapping._future
+        return iter(self._mapping.history)
 
 
 class WindowDictValuesView(ValuesView):
     def __contains__(self, value):
-        for rev, v in self._mapping._past:
-            if v == value:
-                return True
-        for rev, v in self._mapping._future:
+        for (rev, v) in self._mapping.history:
             if v == value:
                 return True
         return False
 
     def __iter__(self):
-        for rev, v in self._mapping._past:
-            yield v
-        for rev, v in self._mapping._future:
+        for rev, v in self._mapping.history:
             yield v
 
 
@@ -53,26 +36,39 @@ class WindowDict(MutableMapping):
     Optimized for the cases where you look up the same revision repeatedly, or its neighbors.
     
     """
-    __slots__ = ['_past', '_future']
+    __slots__ = ['history']
 
     def seek(self, rev):
         """Arrange the caches in the optimal way for looking up the given revision."""
         # TODO: binary search? Perhaps only when one or the other deque is very large?
-        while self._future and self._future[0][0] <= rev:
-            self._past.append(self._future.popleft())
-        while self._past and self._past[-1][0] > rev:
-            self._future.appendleft(self._past.pop())
+        hoplen = len(self.history) // 2
+        currev = self.history.middle[0]
+        while hoplen > 1:
+            if currev == rev:
+                return
+            elif currev > rev:
+                self.history.seek(-hoplen)
+            else:  # currev < rev
+                self.history.seek(hoplen)
+            hoplen //= 2
+            currev = self.history.middle[0]
+        while currev > rev:
+            self.history.seek(-1)
+            currev = self.history.middle[0]
+        while currev < rev:
+            self.history.seek(1)
+            currev = self.history.middle[0]
 
     def rev_before(self, rev):
         """Return the last rev prior to the given one on which the value changed."""
         self.seek(rev)
-        return self._past[-1][0]
+        return self.history.middle[0]
 
     def rev_after(self, rev):
         """Return the next rev after the given one on which the value will change, or None if it never will."""
         self.seek(rev)
-        if self._future:
-            return self._future[0][0]
+        self.history.seek(1)
+        return self.history.middle[0]
 
     def items(self):
         return WindowDictItemsView(self)
@@ -81,69 +77,32 @@ class WindowDict(MutableMapping):
         return WindowDictValuesView(self)
 
     def __init__(self, data={}):
-        self._past = deque(sorted(data.items()))
-        self._future = deque()
+        self.history = Trique(sorted(data.items()))
 
     def __iter__(self):
-        for (rev, v) in self._past:
-            yield rev
-        for (rev, v) in self._future:
+        for (rev, v) in self.history:
             yield rev
 
     def __len__(self):
-        return len(self._past) + len(self._future)
+        return len(self.history)
 
     def __getitem__(self, rev):
         self.seek(rev)
-        if not self._past:
-            raise KeyError("Revision {} is before the start of history".format(rev))
-        return self._past[-1][1]
+        return self.history.middle
 
     def __setitem__(self, rev, v):
-        if not self._past:
-            self._past.append((rev, v))
-        elif rev < self._past[0][0]:
-            self._past.appendleft((rev, v))
-        elif rev == self._past[0][0]:
-            self._past[0] = (rev, v)
-        elif rev == self._past[-1][0]:
-            self._past[-1] = (rev, v)
-        elif rev > self._past[-1][0]:
-            if not self._future or rev < self._future[0][0]:
-                self._past.append((rev, v))
-            elif rev == self._future[0][0]:
-                self._future[0] = (rev, v)
-            elif rev == self._future[-1][0]:
-                self._future[-1] = (rev, v)
-            elif rev > self._future[-1][0]:
-                self._future.append((rev, v))
-            else:
-                self._future.append((rev, v))
-                inserted = sorted(self._future)
-                self._future = deque(inserted)
+        self.seek(rev)
+        if rev == self.history.middle[0]:
+            self.history.middle = (rev, v)
         else:
-            # I was going to implement my own insertion sort here, but I gather Python already
-            # does that, via Timsort. I wonder if there's a way I can give it a hint, so it doesn't
-            # have to check for partial ordering? And maybe avoid reconstructing the deque?
-            self._past.append((rev, v))
-            inserted = sorted(self._past)
-            self._past = deque(inserted)
+            self.history.insertmiddle((rev, v))
     
     def __delitem__(self, rev):
-        name = '_past' if rev <= self._rev else '_future'
-        stack = getattr(self, name)
-        waste = deque()
-        setattr(self, name, waste)
-        deleted = False
-        while stack:
-            (r, v) = stack.popleft()
-            if r != rev:
-                waste.append((r, v))
-            else:
-                assert not deleted
-                deleted = True
-        if not deleted:
-            raise KeyError("Rev not present: {}".format(rev))
+        self.seek(rev)
+        if self.history.middle[0] == rev:
+            self.history.middle = (rev, None)
+        else:
+            self.history.insertmiddle((rev, None))
 
     def __repr__(self):
         return "WindowDict({})".format(repr(dict(self)))
