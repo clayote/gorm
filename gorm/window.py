e9056917d6c4688116306f34e34218acbbc7ac2d
Zachary Spector <zacharyspector@gmail.com>
2016-10-20 13:52:21 -0400

    Make WindowDict.seek() a little bit lazier

--- gorm/window.py
+++ gorm/window.py
@@ -58,7 +58,7 @@ class WindowDict(MutableMapping):
     def seek(self, rev):
         """Arrange the caches in the optimal way for looking up the given revision."""
         # TODO: binary search? Perhaps only when one or the other deque is very large?
-        if self._past and self._future and self._past[-1][0] <= rev < self._future[0][0]:
+        if self._past and self._past[-1][0] <= rev and (not self._future or self._future[0][0] > rev):
             return
         while self._future and self._future[0][0] <= rev:
             self._past.append(self._future.popleft())
